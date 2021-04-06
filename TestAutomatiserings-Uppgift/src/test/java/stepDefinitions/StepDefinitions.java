b912ce4b910ba7a5e1cab4ee6793facb97c28034
darkdusk24 <johan.svensson@utb.ecutbildning.se>
2021-04-06 11:38:53 +0200

    Change to JavaSE-1.8 to be able to use switch statements with strings. And removal of Thread.sleep Sixth commit

--- TestAutomatiserings-Uppgift/src/test/java/stepDefinitions/StepDefinitions.java
+++ TestAutomatiserings-Uppgift/src/test/java/stepDefinitions/StepDefinitions.java
@@ -9,6 +9,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import io.cucumber.java.After;
 import io.cucumber.java.Before;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
@@ -18,18 +19,19 @@ import net.bytebuddy.utility.RandomString;
 public class StepDefinitions {
 
 	private WebDriver driver;
-	
+
 	@Before
-	public void startUp() throws InterruptedException {
+	public void startUp() {
 		System.setProperty("webdriver.chrome.driver", "C:\\Eclipse\\chromedriver.exe");
 		driver = new ChromeDriver(); // Starta chrome
 		driver.manage().window().maximize(); // Maximera fönstret
 		driver.get("https://login.mailchimp.com/signup/");
 		elementFound(By.cssSelector("button#onetrust-accept-btn-handler"));
-		WebElement cookiesAccept = driver.findElement(By.cssSelector("button#onetrust-accept-btn-handler"));
-		elementClickable(By.cssSelector("button#onetrust-accept-btn-handler"));
-		Thread.sleep(1000);
-		cookiesAccept.click();
+	}
+
+	@After
+	public void tearDown() {
+		driver.quit();
 	}
 
 	@Given("I have entered {string} into the e-mail input box")
@@ -45,31 +47,76 @@ public class StepDefinitions {
 	}
 
 	@Given("I have entered {string} into the password input box")
-	public void i_have_entered_password_into_the_password_input_box(String password) throws InterruptedException {
+	public void i_have_entered_password_into_the_password_input_box(String password) {
 		WebElement passwordInputBox = driver.findElement(By.cssSelector("input#new_password"));
 		passwordInputBox.sendKeys(password);
-		Thread.sleep(1000);
 	}
 
 	@When("I press the sign up button")
 	public void i_press_the_sign_up_button() {
+		WebElement cookiesAccept = driver.findElement(By.cssSelector("button#onetrust-accept-btn-handler"));
+		elementClickable(By.cssSelector("button#onetrust-accept-btn-handler"));
+		cookiesAccept.click();
 		WebElement signUpButton = driver.findElement(By.cssSelector("button#create-account"));
 		signUpButton.click();
 	}
 
 	@Then("{string} should be visible on the screen")
-	public void This_should_be_visible_on_the_screen(String outcome) throws InterruptedException {
+	public void This_should_be_visible_on_the_screen(String outcome) {
 		result(outcome);
-		Thread.sleep(1000);
-		driver.quit();
 	}
 
+	private String usernameCreator(String usernameType) {
+		String input = "";
+
+		switch (usernameType) {
+		case "functional":
+			input = RandomString.make(50);
+			break;
+		case "OneHundredOne":
+			input = RandomString.make(101);
+			break;
+		case "alreadyInUse":
+			input = "Testing123";
+			break;
+		}
+		return input;
+	}
+
+	private Boolean result(String outcome) {
+		Boolean successful = false;
+		WebElement outcomeText;
+
+		switch (outcome) {
+		case "Check your email":
+			elementFound(By.cssSelector("h1[class^='!margin-bottom--lv3'"));
+			outcomeText = driver.findElement(By.cssSelector("h1[class^='!margin-bottom--lv3'"));
+			assertEquals(outcome, outcomeText.getText());
+			break;
+		case "Enter a value less than 100 characters long":
+			elementFound(By.xpath("//*[@id=\"signup-form\"]/fieldset/div[2]/div/span"));
+			outcomeText = driver.findElement(By.xpath("//*[@id=\"signup-form\"]/fieldset/div[2]/div/span"));
+			assertEquals(outcome, outcomeText.getText());
+			break;
+		case "Another user with this username already exists. Maybe it's your evil twin. Spooky.":
+			elementFound(By.xpath("//*[@id=\"signup-form\"]/fieldset/div[2]/div/span"));
+			outcomeText = driver.findElement(By.xpath("//*[@id=\"signup-form\"]/fieldset/div[2]/div/span"));
+			assertEquals(outcome, outcomeText.getText());
+			break;
+		case "Please enter a value":
+			elementFound(By.xpath("//*[@id=\"signup-form\"]/fieldset/div[1]/div/span"));
+			outcomeText = driver.findElement(By.xpath("//*[@id=\"signup-form\"]/fieldset/div[1]/div/span"));
+			assertEquals(outcome, outcomeText.getText());
+			break;
+		}
+		return successful;
+	}
+	
 	private void elementFound(By by) {
 
 		(new WebDriverWait(driver, 10)).until(ExpectedConditions.presenceOfElementLocated(by));
 
 		driver.findElement(by);
-
 	}
 
 	private void elementClickable(By by) {
@@ -77,45 +124,6 @@ public class StepDefinitions {
 		(new WebDriverWait(driver, 10)).until(ExpectedConditions.elementToBeClickable(by));
 
 		driver.findElement(by);
-
-	}
-
-	private String usernameCreator(String usernameType) {
-		String input = "";
-
-//		switch(usernameType) {
-//		
-//		}
-
-		if (usernameType.equalsIgnoreCase("functional")) {
-			input = RandomString.make(50);
-		} else if (usernameType.equalsIgnoreCase("OneHundredOne")) {
-			input = RandomString.make(101);
-		} else if (usernameType.equalsIgnoreCase("alreadyInUse")) {
-			input = "Testing123";
-		}
-
-		return input;
-	}
-	
-	private void result(String outcome) {
-		if (outcome.equalsIgnoreCase("Account created")) {
-			elementFound(By.cssSelector("h1[class^='!margin-bottom--lv3'"));
-			WebElement textBanner = driver.findElement(By.cssSelector("h1[class^='!margin-bottom--lv3'"));
-			assertEquals("Check your email", textBanner.getText());
-		} else if (outcome.equalsIgnoreCase("Too long username")) {
-			elementFound(By.xpath("//*[@id=\"signup-form\"]/fieldset/div[2]/div/span"));
-			WebElement errorText = driver.findElement(By.xpath("//*[@id=\"signup-form\"]/fieldset/div[2]/div/span"));
-			assertEquals("Enter a value less than 100 characters long", errorText.getText());
-		} else if (outcome.equalsIgnoreCase("Username already in use")) {
-			elementFound(By.xpath("//*[@id=\"signup-form\"]/fieldset/div[2]/div/span"));
-			WebElement errorText = driver.findElement(By.xpath("//*[@id=\"signup-form\"]/fieldset/div[2]/div/span"));
-			assertEquals("Another user with this username already exists. Maybe it's your evil twin. Spooky.", errorText.getText());
-		} else if (outcome.equalsIgnoreCase("No email inputted")) {
-			elementFound(By.xpath("//*[@id=\"signup-form\"]/fieldset/div[1]/div/span"));
-			WebElement errorText = driver.findElement(By.xpath("//*[@id=\"signup-form\"]/fieldset/div[1]/div/span"));
-			assertEquals("Please enter a value", errorText.getText());
-		}
 	}
 
 }
--- TestAutomatiserings-Uppgift/src/test/resources/Features/UserCreator.feature
+++ TestAutomatiserings-Uppgift/src/test/resources/Features/UserCreator.feature
@@ -1,16 +1,16 @@
 Feature: UserCreator
-To have an easy way for people to contact me I want to make an account on Mailchimp
+Description: To have an easy way for people to contact me I want to make an account on Mailchimp
 
-Scenario Outline: Create User
-Given I have entered <email> into the e-mail input box
-And I have entered <username> into the username input box
-And I have entered <password> into the password input box
-When I press the sign up button
-Then <outcome> should be visible on the screen
+	Scenario Outline: Create User
+		Given I have entered <email> into the e-mail input box
+		And I have entered <username> into the username input box
+		And I have entered <password> into the password input box
+		When I press the sign up button
+		Then <outcomeText> should be visible on the screen
 
 Examples:
-|						email|		username|			password|						outcome|
+|						email|		username|			password|						outcomeText|
 |"Test@mail.com"|"functional"|"Testing-123"|"Check your email"|
 |"Test@mail.com"|"OneHundredOne"|"Testing-123"|"Enter a value less than 100 characters long"|
 |"Test@mail.com"|"alreadyInUse"|"Testing-123"|"Another user with this username already exists. Maybe it's your evil twin. Spooky."|
-|""|"functional"|"Testing-123"|"No email inputted"|
+|""|"functional"|"Testing-123"|"Please enter a value"|
