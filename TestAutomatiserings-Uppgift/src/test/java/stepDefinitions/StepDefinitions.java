e2b9603a0fc2e7a34905c9642f7473be5ade9a79
darkdusk24 <johan.svensson@utb.ecutbildning.se>
2021-04-06 12:10:32 +0200

    Creating methods to sendkeys, getText and click to/on elements Seventh commit

--- TestAutomatiserings-Uppgift/src/test/java/stepDefinitions/StepDefinitions.java
+++ TestAutomatiserings-Uppgift/src/test/java/stepDefinitions/StepDefinitions.java
@@ -4,7 +4,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -36,34 +35,29 @@ public class StepDefinitions {
 
 	@Given("I have entered {string} into the e-mail input box")
 	public void i_have_entered_into_the_e_mail_input_box(String email) {
-		WebElement emailInputBox = driver.findElement(By.cssSelector("input#email"));
-		emailInputBox.sendKeys(email);
+		sendKeys(By.cssSelector("input#email"), email);
 	}
 
 	@Given("I have entered {string} into the username input box")
 	public void i_have_entered_into_the_username_input_box(String usernameType) {
-		WebElement usernameInputBox = driver.findElement(By.cssSelector("input#new_username"));
-		usernameInputBox.sendKeys(usernameCreator(usernameType));
+		sendKeys(By.cssSelector("input#new_username"), usernameCreator(usernameType));
 	}
 
 	@Given("I have entered {string} into the password input box")
 	public void i_have_entered_password_into_the_password_input_box(String password) {
-		WebElement passwordInputBox = driver.findElement(By.cssSelector("input#new_password"));
-		passwordInputBox.sendKeys(password);
+		sendKeys(By.cssSelector("input#new_password"), password);
 	}
 
 	@When("I press the sign up button")
 	public void i_press_the_sign_up_button() {
-		WebElement cookiesAccept = driver.findElement(By.cssSelector("button#onetrust-accept-btn-handler"));
-		elementClickable(By.cssSelector("button#onetrust-accept-btn-handler"));
-		cookiesAccept.click();
-		WebElement signUpButton = driver.findElement(By.cssSelector("button#create-account"));
-		signUpButton.click();
+		click(By.cssSelector("button#onetrust-accept-btn-handler"));
+		click(By.cssSelector("button#create-account"));
 	}
 
 	@Then("{string} should be visible on the screen")
 	public void This_should_be_visible_on_the_screen(String outcome) {
-		result(outcome);
+		Boolean successful = result(outcome);
+		assertEquals(true, successful);
 	}
 
 	private String usernameCreator(String usernameType) {
@@ -85,28 +79,31 @@ public class StepDefinitions {
 
 	private Boolean result(String outcome) {
 		Boolean successful = false;
-		WebElement outcomeText;
 
 		switch (outcome) {
 		case "Check your email":
 			elementFound(By.cssSelector("h1[class^='!margin-bottom--lv3'"));
-			outcomeText = driver.findElement(By.cssSelector("h1[class^='!margin-bottom--lv3'"));
-			assertEquals(outcome, outcomeText.getText());
+			assertEquals(outcome, getText(By.cssSelector("h1[class^='!margin-bottom--lv3'")));
+			if(outcome.equals(getText(By.cssSelector("h1[class^='!margin-bottom--lv3'"))))
+				successful = true;
 			break;
 		case "Enter a value less than 100 characters long":
 			elementFound(By.xpath("//*[@id=\"signup-form\"]/fieldset/div[2]/div/span"));
-			outcomeText = driver.findElement(By.xpath("//*[@id=\"signup-form\"]/fieldset/div[2]/div/span"));
-			assertEquals(outcome, outcomeText.getText());
+			assertEquals(outcome, getText(By.xpath("//*[@id=\"signup-form\"]/fieldset/div[2]/div/span")));
+			if(outcome.equals(getText(By.xpath("//*[@id=\"signup-form\"]/fieldset/div[2]/div/span"))))
+				successful = true;
 			break;
 		case "Another user with this username already exists. Maybe it's your evil twin. Spooky.":
 			elementFound(By.xpath("//*[@id=\"signup-form\"]/fieldset/div[2]/div/span"));
-			outcomeText = driver.findElement(By.xpath("//*[@id=\"signup-form\"]/fieldset/div[2]/div/span"));
-			assertEquals(outcome, outcomeText.getText());
+			assertEquals(outcome, getText(By.xpath("//*[@id=\"signup-form\"]/fieldset/div[2]/div/span")));
+			if(outcome.equals(getText(By.xpath("//*[@id=\"signup-form\"]/fieldset/div[2]/div/span"))))
+				successful = true;
 			break;
 		case "Please enter a value":
 			elementFound(By.xpath("//*[@id=\"signup-form\"]/fieldset/div[1]/div/span"));
-			outcomeText = driver.findElement(By.xpath("//*[@id=\"signup-form\"]/fieldset/div[1]/div/span"));
-			assertEquals(outcome, outcomeText.getText());
+			assertEquals(outcome, getText(By.xpath("//*[@id=\"signup-form\"]/fieldset/div[1]/div/span")));
+			if(outcome.equals(getText(By.xpath("//*[@id=\"signup-form\"]/fieldset/div[1]/div/span"))))
+				successful = true;
 			break;
 		}
 		return successful;
@@ -119,11 +116,26 @@ public class StepDefinitions {
 		driver.findElement(by);
 	}
 
-	private void elementClickable(By by) {
+	private void click(By by) {
 
 		(new WebDriverWait(driver, 10)).until(ExpectedConditions.elementToBeClickable(by));
 
-		driver.findElement(by);
+		driver.findElement(by).click();
+	}
+	
+	private void sendKeys(By by, String keys) {
+
+		(new WebDriverWait(driver, 10)).until(ExpectedConditions.presenceOfElementLocated(by));
+
+		driver.findElement(by).sendKeys(keys);
+	}
+	
+	private String getText(By by) {
+
+		(new WebDriverWait(driver, 10)).until(ExpectedConditions.presenceOfElementLocated(by));
+
+		String text = driver.findElement(by).getText();
+		return text;
 	}
 
 }
